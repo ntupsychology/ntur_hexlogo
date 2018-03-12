d02020cce1e7aac4de275372ce95a31a2ea8f213
Mark Andrews <mjandrews.net@gmail.com>
2018-03-12 17:04:35 +0000

    Initialize repository

---
+++ ntur_logo.R
@@ -0,0 +1,9 @@
+library(hexSticker)
+library(ggplot2)
+
+p <- ggplot(aes(x = mpg, y = wt), data = mtcars) + geom_point(size=0.5)
+p <- p + theme_void() + theme_transparent()
+
+sticker(p, package="ntur", p_size=50, s_x=1, s_y=.75, s_width=1.0, s_height=0.7, 
+        h_color ="darkblue", h_fill="#ed1266", url='ntupsychology.github.io/ntur', u_size = 4,
+        filename="ntur.png")
---
+++ readme.md
@@ -0,0 +1,2 @@
+If you're going to have R package, if *have* to have a hexagonal shaped logo. Those are just the rules.
+
