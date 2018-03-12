f7d0fb43da81c20471d81e82185f4e9c8b0af495
Mark Andrews <mjandrews.net@gmail.com>
2018-03-12 20:26:44 +0000

    Update colours on logo
    * Also update the image.ibb.co url in readme.md

--- ntur_logo.R
+++ ntur_logo.R
@@ -5,5 +5,5 @@ p <- ggplot(aes(x = mpg, y = wt), data = mtcars) + geom_point(size=0.5)
 p <- p + theme_void() + theme_transparent()
 
 sticker(p, package="ntur", p_size=50, s_x=1, s_y=.75, s_width=1.0, s_height=0.7, 
-        h_color ="darkblue", h_fill="#ed1266", url='ntupsychology.github.io/ntur', u_size = 4,
+        h_color ="#043b64", h_fill="#e3045c", url='ntupsychology.github.io/ntur', u_size = 4,
         filename="ntur.png")
--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
 If you're going to have R package, you *have* to have a hexagonal shaped logo. Those are just the rules of R.
 
-![nturlogo](https://image.ibb.co/iSHgDS/ntur.png "ntur logo")\
-
-
+![nturlogo](https://image.ibb.co/ex5RV7/ntur.png "ntur logo")\
